22667f5e5e6eb6a1e3e50d75b4289a6f99d6bc49
Perro2110 <perrottamarco2011@gmail.com>
2026-04-09 22:58:40 +0200

    fix many part of pipeline

--- notebooks/02_SHARE_training.jl
+++ notebooks/02_SHARE_training.jl
@@ -28,6 +28,11 @@ begin
     using Random
     using StatsBase
     using PlutoUI
+
+	using SoleModels
+	import PlutoUI: combine
+	scitype_check_level=0
+	
 end
 
 # ╔═╡ a2000000-3353-11f1-90b2-21952756a80b
@@ -39,12 +44,15 @@ end
 # ╔═╡ a3000000-3353-11f1-90b2-21952756a80b
 data = deserialize(SERIALIZE_PATH)
 
+# ╔═╡ ab344996-dcc4-49ef-a30c-200761f7c4eb
+count(x-> ismissing(x), data[:,:euro_d])
+
 # ╔═╡ a5000000-3353-11f1-90b2-21952756a80b
 begin
-    y = coerce(data[:, :euro_d], Multiclass)
+    # Rimuovi le righe con missing nel target PRIMA di tutto
+	y = coerce(string.(data[:, :euro_d]), Multiclass)
     X_raw = select(data, Not(:euro_d))
 
-    # Coerci automaticamente in base al tipo attuale
     coerce_dict = Dict{Symbol, Type}()
     for col in names(X_raw)
         st = scitype(X_raw[:, col])
@@ -60,13 +68,19 @@ begin
 end
 
 # ╔═╡ a6000000-3353-11f1-90b2-21952756a80b
-# SO CHE NOI GIA ARRIVIAMO DA PRE PROCESSING MA VOI LO FACEVATE
+# TODO COMMENTARE E METTERE WARNING PARLANDO DI FillImputer E SPIEGARE BENE IL FATTO CHE è MULTIVARIATO.
+
 begin
     imputer = FillImputer()
     imp_mach = machine(imputer, X_coerced)
     fit!(imp_mach)
     X = MLJ.transform(imp_mach, X_coerced)
     schema(X)
+end
+
+# ╔═╡ e44a984c-97e7-48b4-9eae-658f141773c4
+begin
+    y 
 end
 
 # ╔═╡ a7000000-3353-11f1-90b2-21952756a80b
@@ -88,10 +102,11 @@ Classi: **$(unique(y))**
 """
 
 # ╔═╡ b0000000-3353-11f1-90b2-21952756a80b
+# todo attualmente e sbilanciato e non lo teniamo di conto 
 begin
     (X_train, X_test), (y_train, y_test) = partition(
         (X, y), 0.7;
-        rng=121, shuffle=true, multi=true
+        rng=1605, shuffle=true,multi=true
     )
 end
 
@@ -100,22 +115,37 @@ begin
     try
         DecisionTreeClassifier = @load DecisionTreeClassifier pkg=DecisionTree
     catch
-        println("DecisionTreeClassifier già importato.")
+        println("DecisionTreeClassifier already imported.")
     end
 end
 
 # ╔═╡ b2000000-3353-11f1-90b2-21952756a80b
 md"## Model Training"
 
+# ╔═╡ c1ea8a00-0772-4717-af4b-cf848a825a10
+possible_depths = collect(1:10)
+
+# ╔═╡ 6d24d012-d0ba-4cfa-80a3-2126833c7f80
+begin
+	models = []
+	for d in possible_depths
+		model = MLJDecisionTreeInterface.DecisionTreeClassifier(
+	    	max_depth         = d,
+	    	min_samples_leaf  = 1,
+	    	min_samples_split = 2
+		)
+		push!(models,model)
+	end
+end
+
 # ╔═╡ b3000000-3353-11f1-90b2-21952756a80b
-@bind max_depth_value Slider(2:10, default=5, show_value=true)
+@bind max_depth_value Slider(possible_depths, default=5, show_value=true)
+
+# ╔═╡ 6fa2ba7c-720a-46be-a2ac-4a28115b4d84
+##TODO: spieghiamo varie robe 
 
 # ╔═╡ b4000000-3353-11f1-90b2-21952756a80b
-model = MLJDecisionTreeInterface.DecisionTreeClassifier(
-    max_depth         = max_depth_value,
-    min_samples_leaf  = 1,
-    min_samples_split = 2
-)
+model = models[max_depth_value]
 
 # ╔═╡ b5000000-3353-11f1-90b2-21952756a80b
 begin
@@ -129,6 +159,9 @@ end
 # ╔═╡ b6000000-3353-11f1-90b2-21952756a80b
 cm_dt
 
+# ╔═╡ 31eb4188-2a5a-42f3-8533-c56c62f0bbaa
+## VOGLIAMO ALLORA FARE PARTISION BILANCIATI 
+
 # ╔═╡ b7000000-3353-11f1-90b2-21952756a80b
 md"**Accuracy Decision Tree (test set):** $(round(accuracy(cm_dt), digits=4))"
 
@@ -137,7 +170,7 @@ begin
     mach_cv = machine(model, X, y)
     acc_cv = evaluate!(
         mach_cv;
-        resampling = StratifiedCV(nfolds=5, shuffle=true),
+        resampling = StratifiedCV(nfolds=10, shuffle=true),
         measures   = [accuracy],
         verbosity  = 0
     )
@@ -148,32 +181,35 @@ end
 md"## Hyperparameter Tuning"
 
 # ╔═╡ c0000000-3353-11f1-90b2-21952756a80b
-begin
-    max_depth_range         = range(model, :max_depth,          lower=2, upper=10)
-    min_samples_leaf_range  = range(model, :min_samples_leaf,   lower=1, upper=5)
-    min_samples_split_range = range(model, :min_samples_split,  lower=2, upper=10)
-
-    tuned_tree = TunedModel(
-        model      = MLJDecisionTreeInterface.DecisionTreeClassifier(),
-        resampling = StratifiedCV(nfolds=5, shuffle=true),
-        range      = [max_depth_range, min_samples_leaf_range, min_samples_split_range],
-        measure    = accuracy,
-        tuning     = Grid()
-    )
-
-    mach_tuned = machine(tuned_tree, X, y)
-    fit!(mach_tuned, verbosity=0)
-
-    y_prob_tuned = MLJ.predict(mach_tuned, X_test)
-    y_pred_tuned = mode.(y_prob_tuned)
-    cm_tuned     = confusion_matrix(y_pred_tuned, y_test)
-end
+ begin
+     max_depth_range         = range(model, :max_depth,          lower=2, upper=10)
+     min_samples_leaf_range  = range(model, :min_samples_leaf,   lower=1, upper=5)
+     min_samples_split_range = range(model, :min_samples_split,  lower=2, upper=10)
+ 
+     tuned_tree = TunedModel(
+         model      = MLJDecisionTreeInterface.DecisionTreeClassifier(),
+         resampling = StratifiedCV(nfolds=10, shuffle=true),
+         range      = [max_depth_range, min_samples_leaf_range, min_samples_split_range],
+         measure    = accuracy,
+         tuning     = RandomSearch()
+     )
+ 
+     mach_tuned = machine(tuned_tree, X, y)
+     fit!(mach_tuned, verbosity=0)
+ 
+     y_prob_tuned = MLJ.predict(mach_tuned, X_test)
+     y_pred_tuned = mode.(y_prob_tuned)
+     cm_tuned     = confusion_matrix(y_pred_tuned, y_test)
+ end
 
 # ╔═╡ c1000000-3353-11f1-90b2-21952756a80b
 report(mach_tuned).best_model
 
 # ╔═╡ c2000000-3353-11f1-90b2-21952756a80b
 md"**Accuracy Tuned DT (test set):** $(round(accuracy(cm_tuned), digits=4))"
+
+# ╔═╡ 5117a6c9-b090-4d85-9cbc-9500beb09f7e
+#a = SoleModels.solemodel(fitted_params(mach_dt).tree)
 
 # ╔═╡ c3000000-3353-11f1-90b2-21952756a80b
 md"## Random Forest"
@@ -187,13 +223,42 @@ begin
     end
 end
 
+# ╔═╡ b6bc4920-7538-4df9-8295-4730db58be77
+begin
+	possible_tree_depths  = collect(1:10)
+	possible_tree_numbers = collect(2:20)
+end
+
+# ╔═╡ b03265a4-0776-41c9-846a-5e74b459814d
+function pollo(directions::Vector)
+	
+	return combine() do Child
+		
+		inputs = [
+			md""" $(name): $(
+				Child(name, Slider(1:100 , show_value = true))
+			)"""
+			
+			for name in directions
+		]
+		
+		md"""
+		#### Wind speeds
+		$(inputs)
+		"""
+	end
+end
+
+# ╔═╡ 1d0cc054-5c38-46d5-9033-4872d265c0d8
+@bind trees_param pollo(["max_depth", "min_samples_leaf", "min_samples_split", "n_trees"]) 
+
 # ╔═╡ c5000000-3353-11f1-90b2-21952756a80b
 begin
     forest = MLJDecisionTreeInterface.RandomForestClassifier(
-        max_depth         = 3,
-        min_samples_leaf  = 1,
-        min_samples_split = 2,
-        n_trees           = 10
+        max_depth         = trees_param.max_depth,
+        min_samples_leaf  = trees_param.min_samples_leaf,
+        min_samples_split = trees_param.min_samples_split,
+        n_trees           = trees_param.n_trees
     )
 
     mach_rf = machine(forest, X_train, y_train)
@@ -221,31 +286,52 @@ md"""
 | Random Forest | $(round(accuracy(cm_rf), digits=4)) |
 """
 
+# ╔═╡ 8d28f8b0-1165-4ed2-bd3e-a09741363e83
+md"""
+# TODO
+----
+- SEPARARE BENE INPUTE NEL NOTEBOOK PRIMA 
+- ORDINARE E COMMENTARE QUESTO CODICE  
+- SOLEMODELS
+- POSTHOC
+"""
+
 # ╔═╡ Cell order:
 # ╠═a1000000-3353-11f1-90b2-21952756a80b
 # ╠═a2000000-3353-11f1-90b2-21952756a80b
 # ╠═a3000000-3353-11f1-90b2-21952756a80b
+# ╠═ab344996-dcc4-49ef-a30c-200761f7c4eb
 # ╠═a5000000-3353-11f1-90b2-21952756a80b
 # ╠═a6000000-3353-11f1-90b2-21952756a80b
+# ╠═e44a984c-97e7-48b4-9eae-658f141773c4
 # ╠═a7000000-3353-11f1-90b2-21952756a80b
-# ╟─a8000000-3353-11f1-90b2-21952756a80b
+# ╠═a8000000-3353-11f1-90b2-21952756a80b
 # ╟─a9000000-3353-11f1-90b2-21952756a80b
 # ╠═b0000000-3353-11f1-90b2-21952756a80b
 # ╠═b1000000-3353-11f1-90b2-21952756a80b
 # ╟─b2000000-3353-11f1-90b2-21952756a80b
+# ╠═c1ea8a00-0772-4717-af4b-cf848a825a10
+# ╠═6d24d012-d0ba-4cfa-80a3-2126833c7f80
 # ╠═b3000000-3353-11f1-90b2-21952756a80b
+# ╠═6fa2ba7c-720a-46be-a2ac-4a28115b4d84
 # ╠═b4000000-3353-11f1-90b2-21952756a80b
 # ╠═b5000000-3353-11f1-90b2-21952756a80b
 # ╠═b6000000-3353-11f1-90b2-21952756a80b
+# ╠═31eb4188-2a5a-42f3-8533-c56c62f0bbaa
 # ╟─b7000000-3353-11f1-90b2-21952756a80b
 # ╠═b8000000-3353-11f1-90b2-21952756a80b
 # ╟─b9000000-3353-11f1-90b2-21952756a80b
 # ╠═c0000000-3353-11f1-90b2-21952756a80b
 # ╠═c1000000-3353-11f1-90b2-21952756a80b
-# ╟─c2000000-3353-11f1-90b2-21952756a80b
+# ╠═c2000000-3353-11f1-90b2-21952756a80b
+# ╠═5117a6c9-b090-4d85-9cbc-9500beb09f7e
 # ╟─c3000000-3353-11f1-90b2-21952756a80b
 # ╠═c4000000-3353-11f1-90b2-21952756a80b
+# ╠═b6bc4920-7538-4df9-8295-4730db58be77
+# ╠═b03265a4-0776-41c9-846a-5e74b459814d
+# ╠═1d0cc054-5c38-46d5-9033-4872d265c0d8
 # ╠═c5000000-3353-11f1-90b2-21952756a80b
 # ╠═c6000000-3353-11f1-90b2-21952756a80b
 # ╟─c7000000-3353-11f1-90b2-21952756a80b
 # ╟─c8000000-3353-11f1-90b2-21952756a80b
+# ╠═8d28f8b0-1165-4ed2-bd3e-a09741363e83
