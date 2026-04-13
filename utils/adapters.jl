4bd2afc0fa3c5d6e4cb8295ab4f39dad326e064d
mauro-milella <milella_mauro@live.it>
2026-04-13 16:43:32 +0200

    [ADD] mutual information

--- utils/adapters.jl
+++ utils/adapters.jl
@@ -1,13 +1,55 @@
 using StatsBase
 
 """
-Just an adapter for `StatsBase.entropy`, which works only for probability
-distributions (i.e., values ranging between 0 and 1).
+Perform binning if the given collection is not categorical.
+"""
+function maybe_bin(x; nbins=10)
+    # do nothing if the collection is already categorical
+    if !(eltype(x) <: Number)
+        return x
+    end
+
+    ux = unique(x)
+    if length(ux) ≤ nbins
+        return x
+    end
+
+    edges = quantile(x, range(0, 1; length=nbins+1))
+    return cut(x, edges; labels=1:nbins, extend=true)
+end
+
+"""
+This is just an adapter for `StatsBase.entropy`, which works only for 
+probability distributions (i.e., values in [0, 1] and summing to 1).
 """
 function entropy(col)
     counts = values(countmap(col))
     probs = collect(counts) ./ sum(counts)
     return StatsBase.entropy(probs)
+end
+
+"""
+See [`mutual_information`](@ref).
+"""
+function joint_entropy(x, y)
+    joint_counts = values(countmap(zip(x, y)))
+    probs = collect(joint_counts) ./ sum(joint_counts)
+    return StatsBase.entropy(probs)
+end
+
+"""
+Computes the mutual information I(X,Y) = H(Y) - H(Y | X) = H(X) + H(Y) - H(X|Y)
+where H is the [`entropy`](@ref) function.
+"""
+function mutual_information(x, y; nbins=10)
+    x_bin = maybe_bin(x; nbins=nbins)
+    y_bin = maybe_bin(y; nbins=nbins)
+
+    hx = entropy(x_bin)
+    hy = entropy(y_bin)
+    hxy = joint_entropy(x_bin, y_bin)
+
+    return hx + hy - hxy
 end
 
 """
