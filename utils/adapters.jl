4a1b34b92c54d7c55c3b96689c758bcb7e2de75d
mauro-milella <milella_mauro@live.it>
2026-04-13 18:24:40 +0200

    [ADD] mutual_information

--- utils/adapters.jl
+++ utils/adapters.jl
@@ -4,18 +4,29 @@ using StatsBase
 Perform binning if the given collection is not categorical.
 """
 function maybe_bin(x; nbins=10)
-    # do nothing if the collection is already categorical
+    # if data is categorical, do nothing
     if !(eltype(x) <: Number)
         return x
     end
 
     ux = unique(x)
+    
+    # if data is a float collection but there are just a few unique values,
+    # treat it as a categorical
     if length(ux) ≤ nbins
         return x
     end
 
-    edges = quantile(x, range(0, 1; length=nbins+1))
-    return cut(x, edges; labels=1:nbins, extend=true)
+    qs = range(0, 1; length=nbins + 1)
+    edges = unique(quantile(x, qs))
+
+    # ensure the edges of the binning are valid
+    if length(edges) ≤ 2
+        return x
+    end
+
+    # assign bin labels 
+    return cut(x, edges; labels=1:(length(edges)-1), extend=true)
 end
 
 """
@@ -57,10 +68,10 @@ Coerce the types of a generic dataframe, in such a way that they are suitable
 to be trated by MLJ and Sole.jl.
 """
 function coerce_dataframe(X::AbstractDataFrame)
-    coerce_dict = Dict{Symbol, Type}()
+    coerce_dict = Dict{Symbol,Type}()
 
     for col in names(X)
-		coerce_dict[Symbol(col)] = eltype(scitype(X[:, col]))
+        coerce_dict[Symbol(col)] = eltype(scitype(X[:, col]))
     end
 
     return coerce(X, coerce_dict...)
