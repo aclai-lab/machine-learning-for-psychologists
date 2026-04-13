c9b2b496710827f294fc66e3a93b74740203075b
mauro-milella <milella_mauro@live.it>
2026-04-13 14:53:20 +0200

    spacing

--- utils/measures.jl
+++ utils/measures.jl
@@ -1,13 +1,13 @@
 using StatisticalMeasures
 
-function precision(cm::T) where {T <: StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
+function precision(cm::T) where {T<:StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
     m = cm.mat
     tp = m[2, 2]
     fp = m[1, 2]
     return tp / (tp + fp)
 end
 
-function recall(cm::T) where {T <: StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
+function recall(cm::T) where {T<:StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
     m = cm.mat
     tp = m[2, 2]
     fn = m[2, 1]
