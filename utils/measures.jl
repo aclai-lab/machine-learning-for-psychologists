b180df919803001995b5eab936ebf638daaf48f2
mauro-milella <milella_mauro@live.it>
2026-04-15 00:33:22 +0200

    [FIX] precision and recall worked for rotated confusion matrixes

--- utils/measures.jl
+++ utils/measures.jl
@@ -2,14 +2,14 @@ using StatisticalMeasures
 
 function precision(cm::T) where {T<:StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
     m = cm.mat
-    tp = m[2, 2]
-    fp = m[1, 2]
+    tp = m[1, 1]
+    fp = m[2, 1]
     return tp / (tp + fp)
 end
 
 function recall(cm::T) where {T<:StatisticalMeasures.ConfusionMatrices.ConfusionMatrix}
     m = cm.mat
-    tp = m[2, 2]
-    fn = m[2, 1]
+    tp = m[1, 1]
+    fn = m[1, 2]
     return tp / (tp + fn)
 end
